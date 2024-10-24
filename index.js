a4d989a231aa87f2e937fae0d7a2977b1b34ecf5
eetumaki <eetumaki01@gmail.com>
2024-10-24 16:00:44 +0300

    branch created. connected the app to mongodb atlas.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
+// Load enviroment variables from .env file
+dotenv.config()
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Atlas connected!'))
+  .catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
