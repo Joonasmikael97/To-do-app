16959af95f7efe3196c8c03a2085bf33d9bdbef5
eetumaki <eetumaki01@gmail.com>
2024-10-24 16:02:47 +0300

    added comment.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ dotenv.config()
 
 app.use(express.json());
 
+
+// Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Atlas connected!'))
   .catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
